b6c832ccd64d35744db6db9df5066829fb17df70
Bipin <bipingowda7@gmail.com>
2024-10-21 16:57:17 +0530

    updated changes in socket

--- index.js
+++ index.js
@@ -47,6 +47,9 @@ const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
     origin: "https://letsdogossips.netlify.app/",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   },
 });
 
